9a8abc418f53169422a45959f2d112ea740b7dac
Spencer Chang <spencerc99@gmail.com>
2023-05-22 19:53:53 -0700

    working every color bot

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import bsky from '@atproto/api';
 const { BskyAgent } = bsky;
 import * as dotenv from 'dotenv';
 import process from 'node:process';
+import { createCanvas } from 'canvas';
+import { CronJob } from 'cron';
+
 dotenv.config();
 
 const agent = new BskyAgent({
@@ -13,26 +16,44 @@ await agent.login({
   password: process.env.BSKY_PASSWORD!,
 });
 
-const bleet = 'You can find the code for this bleet >>>here<<<, with a link card, a title and a description!';
-await agent.post({
-  text: bleet,
-  facets: [
-    {
-      index: { byteStart: bleet.indexOf('>>>') + 3, byteEnd: bleet.indexOf('<<<') },
-      features: [
-        {
-          $type: 'app.bsky.richtext.facet#link',
-          uri: 'https://github.com/aliceisjustplaying/atproto-starter-kit',
-        }
-      ]
-    }
-  ],
-  embed: {
-    $type: 'app.bsky.embed.external',
-    external: {
-      uri: 'https://github.com/aliceisjustplaying/atproto-starter-kit',
-      title: "alice's atproto starter kit",
-      description: "i'm just playing around with the api",
+// Every color bot
+// const randomNumber = seedrandom('colorbot');
+// generates a random hex color
+function generateRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function createColorSquare(color: string): Buffer {
+  // create a square with the color using canvas and returns it
+  const canvas = createCanvas(100, 100);
+  canvas.width = 100;
+  canvas.height = 100;
+  const ctx = canvas.getContext('2d')!;
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, 100, 100);
+  return canvas.toBuffer();
+}
+
+async function createAndPostImage() {
+  const newColor = generateRandomColor();
+  const image = createColorSquare(newColor);
+  console.log(newColor);
+  // console.log(image);
+
+  // from https://gist.github.com/trozzelle/38ac90c5721c1fa1b6de488f8ca0eb11
+  const blob = await agent.uploadBlob(image, { encoding: 'image/png' });
+  await agent.post({
+    text: `${newColor}`,
+    embed: {
+      $type: 'app.bsky.embed.images',
+      images: [{ image: blob.data.blob, alt: `a square filled with the color ${newColor}.` }],
     },
-  },
-});
+  });
+}
+
+// Run this on a cron job
+const scheduleExpression = '0 */3 * * *'; // Run once every three hours in prod
+
+const job = new CronJob(scheduleExpression, createAndPostImage);
+
+job.start();
